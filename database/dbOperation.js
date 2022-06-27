87e70b25ec6f693b639ab994710a5c5158512f15
seba-nigu <seba80443@gmail.com>
2022-06-28 01:12:19 +0300

    add register and login

--- database/dbOperation.js
+++ database/dbOperation.js
@@ -1,2 +1,33 @@
 const config = require("./dbConfig");
 const sql = require("mssql");
+
+const createRegister = async (Login) => {
+  try {
+    let pool = await sql.connect(config);
+    let data = await pool
+      .request()
+      .query(
+        `INSERT INTO [user] VALUES('${Login.username}', '${Login.email}', '${Login.password}');`
+      );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getLogin = async (email) => {
+  try {
+    let pool = await sql.connect(config);
+    let data = pool
+      .request()
+      .query(`SELECT * FROM [user] WHERE email ='${email}'`);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  createRegister,
+  getLogin,
+};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 const app = express();
 const dbOperation = require("../database/dbOperation");
 app.use(express.json());
 app.use(cors());
+
+app.post("/api/register", (req, res) => {
+  bcrypt.hash(req.body.password, 10, async function (err, hash) {
+    await dbOperation.createRegister({
+      email: req.body.email,
+      username: req.body.username,
+      password: hash,
+    });
+  });
+  res.send("Register successfull");
+});
+
+app.post("/api/login", async (req, res) => {
+  // check in dbOperation if user and password match
+  let data = await dbOperation.getLogin(req.body.email);
+  bcrypt.compare(
+    req.body.password,
+    data.recordset[0].password,
+    function (err, resp) {
+      if (err) {
+        console.log(err);
+      }
+      if (resp) {
+        // Send JWT next
+        console.log("Match");
+      } else {
+        console.log("No match");
+        res.status(400).json("Email or Password incorrect!");
+      }
+    }
+  );
+});
 
 const port = 5000;
 app.listen(port, () => {
