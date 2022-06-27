684603c5088529b1aaf59f66251eaadbe784afed
seba-nigu <seba80443@gmail.com>
2022-06-28 00:30:49 +0300

    add logic to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Login() {
+  // the login data from the form that will be passed to the backend
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+  // the data inside the input set into "login" state
+  const setInput = (e) => {
+    const { name, value } = e.target;
+    setLogin((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const postLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { data: res } = await axios.post(
+        "http://localhost:5000/api/login",
+        {
+          email: login.email,
+          password: login.password,
+        }
+      );
+      localStorage().setItem("token", res.data);
+      console.log(res.message);
+      window.location = "/";
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="login-page h-screen flex justify-center items-center">
       <div className="w-1/3">
         <form
           className="bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4"
-          method="post"
-          action="http://localhost:5000/login"
+          onSubmit={postLogin}
         >
           <div className="mb-4">
             <label className="text-gray-700 text-sm font-bold mb-2">
@@ -16,6 +50,7 @@ function Login() {
               type="email"
               name="email"
               placeholder="Email"
+              onChange={setInput}
             />
           </div>
           <div className="mb-8">
@@ -27,6 +62,7 @@ function Login() {
               type="password"
               name="password"
               placeholder="Password"
+              onChange={setInput}
             />
           </div>
           <div className="flex justify-between flex-col">
@@ -37,18 +73,18 @@ function Login() {
               Login
             </button>
             <div className="flex flex-col mt-4 h-12 justify-between w-40">
-              <a
+              <Link
+                to="/register"
                 className="cursor-pointer font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="/register"
               >
                 New here? SIGN UP
-              </a>
-              <a
+              </Link>
+              <Link
+                to="/forgot"
                 className="cursor-pointer font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="/forgot"
               >
                 Forgot Password?
-              </a>
+              </Link>
             </div>
           </div>
         </form>
