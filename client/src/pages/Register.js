1f45360a2b5bceafe8ce6a38e54b4584db0a13bf
seba-nigu <seba80443@gmail.com>
2022-06-28 00:35:41 +0300

    add register design

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,72 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="register-page h-screen flex justify-center items-center">
+      <div className="w-1/3">
+        <form className="bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="text-gray-700 text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-700"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="text-gray-700 text-sm font-bold mb-2">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-700"
+              type="text"
+              name="username"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-700"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="mb-8">
+            <label className="text-gray-700 text-sm font-bold mb-2">
+              Retype Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-700"
+              type="password"
+              name="confirm-password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex justify-between flex-col">
+            <button
+              type="submit"
+              className="text-center cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Register
+            </button>
+            <Link
+              to="/login"
+              className="mt-4 cursor-pointer font-bold text-sm text-blue-500 hover:text-blue-800"
+            >
+              Already have an account?
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
