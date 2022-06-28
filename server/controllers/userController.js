1d5fc376769804de4aff2aacf68d07c21610cb06
seba-nigu <seba80443@gmail.com>
2022-06-28 12:51:35 +0300

    removed bug where you can register same email address

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,27 @@
 const bcrypt = require("bcrypt");
 const dbOperation = require("../../database/dbOperation");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
-  bcrypt.hash(req.body.password, 10, async function (err, hash) {
-    await dbOperation.createRegister({
-      email: req.body.email,
-      username: req.body.username,
-      password: hash,
-    });
-  });
-  res.status(200).json({ message: "Registered successfully" });
+  const { email, username, password } = req.body;
+  if (!email || !username || !password) {
+    res.status(400).json({ message: "Please add all fields" });
+  } else {
+    // check if email exists
+    let data = await dbOperation.getLogin(req.body.email);
+    if (data.recordset[0]) {
+      res.status(400).json({ message: "Email already exists" });
+    } else {
+      bcrypt.hash(req.body.password, 10, async function (err, hash) {
+        await dbOperation.createRegister({
+          email: req.body.email,
+          username: req.body.username,
+          password: hash,
+        });
+      });
+      res.status(200).json({ message: "Registered successfully" });
+    }
+  }
 };
 
 const loginUser = async (req, res) => {
